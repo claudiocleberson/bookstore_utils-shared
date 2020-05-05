abd3e550d47b758114695aafe389a304b3a73182
claudio.pereira <claudio.pereira@3CSPP001536N>
2020-05-05 15:05:26 -0300

    http Utils package

--- utils/http_utils/http_utils.go
+++ utils/http_utils/http_utils.go
@@ -3,10 +3,18 @@ package http_utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/claudiocleberson/bookstore_utils-shared/utils/rest_err"
 )
 
 func RespondJson(w http.ResponseWriter, statusCode int, body interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(body)
+}
+
+func RespondError(w http.ResponseWriter, err *rest_err.RestErr) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(err.Code)
+	json.NewEncoder(w).Encode(err)
 }
