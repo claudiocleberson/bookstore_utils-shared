cda2d5af00a4a061ccffd4d7bc56414a0f45f0c0
claudio.pereira <claudio.pereira@3CSPP001536N>
2020-05-06 14:08:01 -0300

    refactored logger module

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.12
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/zap v1.15.0
 )
--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -2,14 +2,23 @@ package logger
 
 import (
 	"fmt"
+	"os"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+const (
+	envLogLevel = "LOG_LEVEL"
+)
+
 var (
 	log logger
 )
+
+type bookstoreLogger interface {
+	Printf(format string, v ...interface{})
+}
 
 type logger struct {
 	log *zap.Logger
@@ -19,7 +28,7 @@ func init() {
 
 	logConfig := zap.Config{
 		OutputPaths: []string{"stdout"},
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Level:       zap.NewAtomicLevelAt(getLevel()),
 		EncoderConfig: zapcore.EncoderConfig{
 			LevelKey:     "level",
 			TimeKey:      "time",
@@ -37,10 +46,24 @@ func init() {
 	}
 }
 
-func GetLogger() logger {
+func GetLogger() bookstoreLogger {
 	return log
 }
 
+func getLevel() zapcore.Level {
+
+	switch os.Getenv(envLogLevel) {
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	case "debug":
+		return zap.DebugLevel
+	default:
+		return zap.InfoLevel
+	}
+
+}
 func (l logger) Printf(format string, v ...interface{}) {
 	if len(v) == 0 {
 		Info(format)
