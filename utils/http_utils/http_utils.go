d57f2b521297a696034cde243751a966497f9bfa
claudio.pereira <claudio.pereira@3CSPP001536N>
2020-05-09 10:40:16 -0300

    added toJson method

--- utils/http_utils/http_utils.go
+++ utils/http_utils/http_utils.go
@@ -16,5 +16,5 @@ func RespondJson(w http.ResponseWriter, statusCode int, body interface{}) {
 func RespondError(w http.ResponseWriter, err rest_err.RestErr) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(err.Code())
-	json.NewEncoder(w).Encode(err)
+	json.NewEncoder(w).Encode(err.ToJson())
 }
